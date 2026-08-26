0f8f48937b108edaf49224b4c89067e78970bc4d
beanonim <poland671488@gmail.com>
2026-08-26 18:12:37 +0300

    Delete versus directory

--- versus/pushokguard/user_info.py
+++
@@ -1,7 +0,0 @@
-# инфа о акке пушокгуард
-
-import os
-
-username = os.environ.get('VERSUS_USERNAME', 'developer')
-uuid = os.environ.get('VERSUS_UUID', '0000')
-sub = os.environ.get('VERSUS_SUBSCRIPTION', 'Навсегда')
